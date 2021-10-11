e232baa347257d53122a4baf3ac8443c97af81f1
vorsakha <andreitf.dev@gmail.com>
2021-10-11 08:01:03 -0300

    add user route

---
+++ src/routes/user.ts
@@ -0,0 +1,30 @@
+import express, { Request, Response } from "express";
+const route = express.Router();
+const userModel = require("../../Model/userModel");
+const auth = require("../Middleware/auth");
+
+// Get user by token
+route.get("/", auth, async (req: Request, res: Response) => {
+  try {
+    const user = await userModel.findById(req.user.id).select("-password");
+
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
+});
+
+// Get all users
+route.get("/all", auth, async (req: Request, res: Response) => {
+  try {
+    const user = await userModel.find().select("-password");
+
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
+});
+
+module.exports = route;
