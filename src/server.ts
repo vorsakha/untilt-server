774cdc121a85a40ee556f8de49401b2ca6270e6f
vorsakha <andreitf.dev@gmail.com>
2021-10-11 07:53:01 -0300

    initial server file setup

---
+++ src/server.ts
@@ -0,0 +1,25 @@
+require("dotenv").config();
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+const DB_URL = process.env.DB_URL;
+
+mongoose.connect(DB_URL as string, (error: any) => {
+  if (error) {
+    return console.log(error);
+  }
+
+  console.log("Connected to DB");
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
