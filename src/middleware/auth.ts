3057f5615a8114ec9a83a50c96b1fab7347019e7
vorsakha <andreitf.dev@gmail.com>
2021-10-11 07:58:19 -0300

    add auth middleware

---
+++ src/middleware/auth.ts
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+import { Request, Response, NextFunction } from "express";
+
+module.exports = function (req: Request, res: Response, next: NextFunction) {
+  //-> Get token from header
+  const token = req.header("x-auth-token");
+
+  //-> Check if no token
+  if (!token) {
+    return res.status(401).json({ msg: "No token, authorization denied" });
+  }
+
+  //-> Verify token
+  try {
+    const decoded: any = jwt.verify(token, process.env.JWT_SECRET as string);
+
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: "Token is not valid" });
+  }
+};
